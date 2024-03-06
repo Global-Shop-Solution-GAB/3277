40ace768a31654624cf7a0024db5983e4ad43fed
Service Web <sw@gssmail.com>
2024-03-06 16:19:53 -0600

    Commit changes in C:\Websites\Git\emarlow\3277\ATG_3277_RTR_QT_CONT.sql

---
+++ ATG_3277_RTR_QT_CONT.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: ATG_3277_RTR_QT_CONT
+ * ========================================== */
+CREATE TABLE "ATG_3277_RTR_QT_CONT"(
+ "ID" IDENTITY DEFAULT '0',
+ "PRODUCT_LINE" VARCHAR(2),
+ "USERS_GROUP" VARCHAR(8),
+ "CONTACT_TYPE" VARCHAR(1),
+ UNIQUE ("ID"));
+
+
